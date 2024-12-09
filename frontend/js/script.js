585483ac4d05a8fc10246a9f4ff61d7f753c19e5
Geemullo <vitinhobd15@gmail.com>
2024-12-09 20:13:37 -0300

    change url backend

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -90,7 +90,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket('ws://localhost:8080')
+    websocket = new WebSocket('wss://app-chating.onrender.com')
     websocket.onmessage = processMessage
 }
 
